a29a6f0143247df1dd279e722961f01f6d959703
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2015-12-10 23:21:21 -0500

    Java docs and Todo for BrightnesAction

--- AndroidActionKit/src/api/ashish/androidactionkit/actions/BrightnessAction.java
+++ AndroidActionKit/src/api/ashish/androidactionkit/actions/BrightnessAction.java
@@ -6,17 +6,28 @@ import android.content.Context;
 
 public class BrightnessAction 
 {
+	/**
+	 *  Android Context
+	 */
 	private Context context = null;
 
-
+	/**
+	 * Save the context onCreate
+	 * @param context
+	 */
 	public void onCreate(Context context)
 	{
 		this.context = context;
 	}
 
+	/**
+	 * Perform the Brightness action based on incoming data.
+	 * 
+	 * @param brightnessData
+	 */
 	public void perform(Map<String, Object> brightnessData)
 	{
-
+		// TODO Add different levels of Brightness. Gradient.
 		// Change the Brightness
 		String action = (String)brightnessData.get("action");
 		if ( "HIGH".equals(action) )
@@ -31,7 +42,11 @@ public class BrightnessAction
 		}
 	}
 
+	/**
+	 * Events to be done on Destroy.
+	 */
 	public void onDestroy()
 	{
+		// Do nothing for now.
 	}
 }
